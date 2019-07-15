21044ddf6f796c6d399ab3c1ce1807d6bf0e3476
dfhissd <429246362@qq.com>
2019-07-15 17:39:22 +0800

    已能生成等测试

--- CreateCode/Form1.Designer.cs
+++ CreateCode/Form1.Designer.cs
@@ -28,13 +28,244 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.textBox_Name = new System.Windows.Forms.TextBox();
+            this.textBox_Time = new System.Windows.Forms.TextBox();
+            this.textBox_ID = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.textBox_text = new System.Windows.Forms.TextBox();
+            this.comboBox_size = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.White;
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(30, 17);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(335, 114);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 20;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 22);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(90, 15);
+            this.label5.TabIndex = 18;
+            this.label5.Text = "请输入内容:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(197, 465);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(110, 53);
+            this.button1.TabIndex = 17;
+            this.button1.Text = "生成二维码";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Location = new System.Drawing.Point(366, 17);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(124, 114);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 16;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label4.Location = new System.Drawing.Point(115, 155);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(90, 15);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "二维码大小:";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(30, 196);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(460, 263);
+            this.tabControl1.TabIndex = 21;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.textBox_Name);
+            this.tabPage1.Controls.Add(this.textBox_Time);
+            this.tabPage1.Controls.Add(this.textBox_ID);
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(452, 234);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "格式文本";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // textBox_Name
+            // 
+            this.textBox_Name.Location = new System.Drawing.Point(111, 93);
+            this.textBox_Name.Name = "textBox_Name";
+            this.textBox_Name.Size = new System.Drawing.Size(295, 25);
+            this.textBox_Name.TabIndex = 24;
+            // 
+            // textBox_Time
+            // 
+            this.textBox_Time.Location = new System.Drawing.Point(111, 170);
+            this.textBox_Time.Name = "textBox_Time";
+            this.textBox_Time.Size = new System.Drawing.Size(295, 25);
+            this.textBox_Time.TabIndex = 23;
+            // 
+            // textBox_ID
+            // 
+            this.textBox_ID.Location = new System.Drawing.Point(111, 16);
+            this.textBox_ID.Name = "textBox_ID";
+            this.textBox_ID.Size = new System.Drawing.Size(295, 25);
+            this.textBox_ID.TabIndex = 22;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(25, 180);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 15);
+            this.label3.TabIndex = 21;
+            this.label3.Text = "出货日期:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 103);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 15);
+            this.label2.TabIndex = 20;
+            this.label2.Text = "客户名称:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 15);
+            this.label1.TabIndex = 19;
+            this.label1.Text = "ID编码:";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.textBox_text);
+            this.tabPage2.Controls.Add(this.label5);
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(452, 234);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "普通文本";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // textBox_text
+            // 
+            this.textBox_text.Location = new System.Drawing.Point(66, 52);
+            this.textBox_text.Multiline = true;
+            this.textBox_text.Name = "textBox_text";
+            this.textBox_text.Size = new System.Drawing.Size(349, 157);
+            this.textBox_text.TabIndex = 19;
+            // 
+            // comboBox_size
+            // 
+            this.comboBox_size.FormattingEnabled = true;
+            this.comboBox_size.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10",
+            "11",
+            "12",
+            "13",
+            "14",
+            "15"});
+            this.comboBox_size.Location = new System.Drawing.Point(211, 152);
+            this.comboBox_size.MaxLength = 2;
+            this.comboBox_size.Name = "comboBox_size";
+            this.comboBox_size.Size = new System.Drawing.Size(168, 23);
+            this.comboBox_size.TabIndex = 22;
+            this.comboBox_size.Text = "5";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(512, 536);
+            this.Controls.Add(this.comboBox_size);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label4);
+            this.Name = "Form1";
+            this.Text = "联策qrCode";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.ComboBox comboBox_size;
+        private System.Windows.Forms.TextBox textBox_text;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox_Name;
+        private System.Windows.Forms.TextBox textBox_Time;
+        private System.Windows.Forms.TextBox textBox_ID;
     }
 }
 
--- CreateCode/Form1.cs
+++ CreateCode/Form1.cs
@@ -3,9 +3,13 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using QRCoder;
+using System.IO;
+
 
 namespace CreateCode
 {
@@ -15,5 +19,162 @@ namespace CreateCode
         {
             InitializeComponent();
         }
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        // <summary>
+        // 二维码公共处理类
+        // </summary>
+        public static class QRCoderHelper
+        {
+            // <summary>
+            // 创建二维码返回文件路径名称
+            // </summary>
+            // <param name="plainText">二维码内容</param>
+            public static string CreateQRCodeToFile(string plainText, int size)
+            {
+                try
+                {
+                    string fileName = "";
+                    if (String.IsNullOrEmpty(plainText))
+                    {
+                        return "";
+                    }
+
+                    //二维码文件目录
+                    string filePath = @"C:\Images\QR\";
+                    if (!Directory.Exists(filePath))
+                    {
+                        Directory.CreateDirectory(filePath);
+                    }
+
+                    //创建二维码文件路径名称
+                    fileName = filePath + DateTime.Now.ToString("yyyyMMddHHmmss") + new Random().Next(100, 1000) + ".jpeg";
+
+                    QRCodeGenerator qrGenerator = new QRCoder.QRCodeGenerator();
+                    //QRCodeGenerator.ECCLevel:纠错能力,Q级：约可纠错25%的数据码字
+                    QRCodeData qrCodeData = qrGenerator.CreateQrCode(plainText, QRCodeGenerator.ECCLevel.Q);
+                    QRCode qrcode = new QRCode(qrCodeData);
+                    //Bitmap qrCodeImage = qrcode.GetGraphic(5);
+                    Bitmap qrCodeImage = qrcode.GetGraphic(size, Color.Black, Color.White, false);
+                    qrCodeImage.Save(fileName, ImageFormat.Jpeg);
+                    return fileName;
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception("创建二维码返回文件路径名称方法异常", ex);
+                }
+            }
+        }
+
+        // <summary>
+        // 创建二维码返回byte数组
+        //</summary>
+        // <param name="plainText">二维码内容</param>
+        public static byte[] CreateQRCodeToBytes(string plainText)
+        {
+            try
+            {
+                if (String.IsNullOrEmpty(plainText))
+                {
+                    return null;
+                }
+
+                QRCodeGenerator qrGenerator = new QRCoder.QRCodeGenerator();
+                //QRCodeGenerator.ECCLevel:纠错能力,Q级：约可纠错25%的数据码字
+                QRCodeData qrCodeData = qrGenerator.CreateQrCode(plainText, QRCodeGenerator.ECCLevel.Q);
+                QRCode qrcode = new QRCode(qrCodeData);
+                Bitmap qrCodeImage = qrcode.GetGraphic(5);
+                MemoryStream ms = new MemoryStream();
+                qrCodeImage.Save(ms, ImageFormat.Jpeg);
+                byte[] arr = new byte[ms.Length];
+                ms.Position = 0;
+                ms.Read(arr, 0, (int)ms.Length);
+                ms.Close();
+
+                return arr;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("创建二维码返回byte数组方法异常", ex);
+            }
+        }
+
+        /// <summary>
+        /// 创建二维码返回Base64字符串
+        /// </summary>
+        /// <param name="plainText">二维码内容</param>
+        public static string CreateQRCodeToBase64(string plainText, bool hasEdify = true)
+        {
+            try
+            {
+                string result = "";
+                if (String.IsNullOrEmpty(plainText))
+                {
+                    return "";
+                }
+
+                QRCodeGenerator qrGenerator = new QRCoder.QRCodeGenerator();
+                //QRCodeGenerator.ECCLevel:纠错能力,Q级：约可纠错25%的数据码字
+                QRCodeData qrCodeData = qrGenerator.CreateQrCode(plainText, QRCodeGenerator.ECCLevel.Q);
+                QRCode qrcode = new QRCode(qrCodeData);
+                Bitmap qrCodeImage = qrcode.GetGraphic(5);
+                MemoryStream ms = new MemoryStream();
+                qrCodeImage.Save(ms, ImageFormat.Jpeg);
+                byte[] arr = new byte[ms.Length];
+                ms.Position = 0;
+                ms.Read(arr, 0, (int)ms.Length);
+                ms.Close();
+                if (hasEdify)
+                {
+                    result = "data:image/jpeg;base64," + Convert.ToBase64String(arr);
+                }
+                else
+                {
+                    result = Convert.ToBase64String(arr);
+                }
+                return result;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("创建二维码返回Base64字符串方法异常", ex);
+            }
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            string plainText = "";
+
+            if (tabControl1.SelectedTab == tabPage1) {
+                //表示需要打印格式文本
+                if ((textBox_ID.Text.Trim() == string.Empty)&& (textBox_Name.Text.Trim() == string.Empty)&&(textBox_Time.Text.Trim() == string.Empty))
+                {
+                    pictureBox1.Image = null;
+                    return;
+                }
+                // 生成二维码的内容
+                plainText = "ID编码：" + textBox_ID.Text + "\r\n客户：" + textBox_Name.Text + "\r\n出货日期：" + textBox_Time.Text;
+            }
+            else {
+                //表示需要打印普通格式文本
+                if (textBox_text.Text.Trim() == string.Empty) {
+                    pictureBox1.Image = null;
+                    return;
+                }
+                // 生成二维码的内容
+                plainText = textBox_text.Text;
+
+            }
+            //创建二维码返回文件路径名称
+            string fileName = QRCoderHelper.CreateQRCodeToFile(plainText, Convert.ToInt32(comboBox_size.Text));
+
+            pictureBox1.Image = Image.FromFile(fileName);
+        }
+
+
     }
 }
